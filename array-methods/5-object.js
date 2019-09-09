5645c09ef3f85acf386f4491d762ae251bda4db8
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-09-09 22:21:02 +0530

    complted 5-object.js

--- array-methods/5-object.js
+++ array-methods/5-object.js
@@ -3,17 +3,28 @@ var obj = {
   place: "thehr",
   course: "javascript"
 }
-
+Object.keys(obj) = ["place", "course"]
+Object.values(obj) = ["thehr", "javascript"]
+Object.entries(obj) = [ ["place","thehr"], ["course","javascript"]
 
 
 // 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
-var student = { 
+ var student = { 
   name : "David Rayy", 
   sclass : "VI", 
   rollno : 12
 };
-
+  // print object before deleting rollno
+  console.log(student)
+  // deleting rollno from object
+  delete student.rollno
+  // print object after deleting rollno
+  console.log(student)
 
 
 
 // 3. Write a function to get the length of an object.
+function lengthofObject(obj){
+  return Object.keys(obj);
+}
+lengthofObject(student)
