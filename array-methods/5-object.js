3eebadb63c8d20ce9f5441b2c1f990d660d2bd01
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-09-13 11:42:54 +0530

    updated string methods

--- array-methods/5-object.js
+++ array-methods/5-object.js
@@ -10,9 +10,9 @@ Object.entries(obj) = [ ["place","thehr"], ["course","javascript"]
 
 // 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
  var student = { 
-  name : "David Rayy", 
-  sclass : "VI", 
-  rollno : 12
+  name: "David Rayy", 
+  sclass: "VI", 
+  rollno: 12
 };
   // print object before deleting rollno
   console.log(student)
