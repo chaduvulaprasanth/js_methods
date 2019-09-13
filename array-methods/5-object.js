8c01088b0f6ba2aba7793fbe97a7ecf55b9a9a4f
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-09-13 13:12:02 +0530

    completed 5-object.js

--- array-methods/5-object.js
+++ array-methods/5-object.js
@@ -1,19 +1,19 @@
 // 1. Write a JavaScript program to list the properties and values of a JavaScript object. (Object.keys)
-var obj = {
-  place: "thehr",
-  course: "javascript"
-}
-Object.keys(obj) = ["place", "course"]
-Object.values(obj) = ["thehr", "javascript"]
-Object.entries(obj) = [ ["place","thehr"], ["course","javascript"]
+  var obj = {
+    place: "thehr",
+    course: "javascript"
+  }
+  Object.keys(obj) = ["place", "course"]
+  Object.values(obj) = ["thehr", "javascript"]
+  Object.entries(obj) = [ ["place","thehr"], ["course","javascript"]
 
 
 // 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
- var student = { 
-  name: "David Rayy", 
-  sclass: "VI", 
-  rollno: 12
-};
+    var student = { 
+      name: "David Rayy", 
+      sclass: "VI", 
+      rollno: 12
+    };
   // print object before deleting rollno
   console.log(student)
   // deleting rollno from object
@@ -24,7 +24,7 @@ Object.entries(obj) = [ ["place","thehr"], ["course","javascript"]
 
 
 // 3. Write a function to get the length of an object.
-function lengthofObject(obj){
-  return Object.keys(obj);
-}
-lengthofObject(student)
+  function lengthofObject(obj){
+    return Object.keys(obj);
+  }
+  lengthofObject(student)
